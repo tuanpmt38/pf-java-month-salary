d609556f28613daea420e0a05318ab2957f91b78
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-18 15:57:32 +0700

    [Bài tập] Tính lương nhân viên

--- MonthSalary.java
+++ MonthSalary.java
@@ -1,14 +1,17 @@
+import java.text.DecimalFormat;
 public class MonthSalary {
     public static void main(String[] args) {
         double factor, year, monthyfactor;
         java.util.Scanner sc = new java.util.Scanner(System.in);
+        String pattern = "###,###.###";
+        DecimalFormat df =new DecimalFormat(pattern);
         System.out.println("Enter factor:");
         factor = sc.nextDouble();
         System.out.println("Enter year:");
         year = sc.nextDouble();
         monthyfactor = factor * 4000000 + year * 500000;
         if (factor <= 1) System.out.println("Re-Enter");
-        else if (factor <= 5) System.out.println("Monthy Factor:" + monthyfactor);
+        else if (factor <= 5) System.out.println("Monthy Factor:"+ df.format(monthyfactor)+ " VNĐ");
         else
             System.out.println("Re-Enter");
     }
